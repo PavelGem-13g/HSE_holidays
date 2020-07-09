d776de8b2850acb14ce2988b991c1224c4603866
PavelGem <pashentsev.1@gmail.com>
2020-07-09 21:55:54 +0300

    full first hw

--- sem1Ball/sem1Ball/Assets/Scripts/BallController.cs
+++ sem1Ball/sem1Ball/Assets/Scripts/BallController.cs
@@ -7,8 +7,10 @@ public class BallController : MonoBehaviour
 {
     public float speed = 3.4f;
     public int score;
-    public UnityEvent fiveCoinsCollected = new UnityEvent();
-    public UnityEvent tenCoinsCollected = new UnityEvent();
+    public UnityEvent _5_CoinsCollected = new UnityEvent();
+    public UnityEvent _10_CoinsCollected = new UnityEvent();
+    public UnityEvent _16_CoinsCollected = new UnityEvent();
+    public UnityEvent _23_CoinsCollected = new UnityEvent();
     Restart restarter;
     private Rigidbody rb;
 
@@ -21,14 +23,14 @@ public class BallController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        /*
-        float horizontalAxis = Input.GetAxis("Horizontal");
+
+/*        float horizontalAxis = Input.GetAxis("Horizontal");
         float verticalAxis = Input.GetAxis("Vertical");
 
         rb.AddForce(new Vector3(horizontalAxis, 0, verticalAxis) * speed);*/
 
-        Vector3 gravity = new Vector3(Input.acceleration.x, Input.acceleration.z, Input.acceleration.y)*10f;
-        rb.AddForce(gravity * speed,ForceMode.Acceleration);
+        Vector3 gravity = new Vector3(Input.acceleration.x, Input.acceleration.z, Input.acceleration.y) * 10f;
+        rb.AddForce(gravity * speed, ForceMode.Acceleration);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -40,11 +42,19 @@ public class BallController : MonoBehaviour
             coin.Vanish();
             if (score >= 50)
             {
-                fiveCoinsCollected.Invoke();
+                _5_CoinsCollected.Invoke();
             }
             if (score >= 100)
             {
-                tenCoinsCollected.Invoke();
+                _10_CoinsCollected.Invoke();
+            }
+            if (score >= 160)
+            {
+                _16_CoinsCollected.Invoke();
+            }
+            if (score >= 230)
+            {
+                _23_CoinsCollected.Invoke();
             }
         }
     }
@@ -55,15 +65,21 @@ public class BallController : MonoBehaviour
             restarter.Load();
         }
         if (collision.gameObject.tag == "Elevator")
+        { 
+            collision.gameObject.GetComponent<Elevator>().isUp = true;
+        }
+        if (collision.gameObject.tag == "Elevator" && collision.gameObject.GetComponent<Elevator>().isOn)
         {
             collision.gameObject.GetComponent<AudioSource>().Play();
         }
+
     }
     private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.tag == "Elevator")
         {
             collision.gameObject.GetComponent<AudioSource>().Pause();
+            collision.gameObject.GetComponent<Elevator>().isUp = false;
         }
     }
 }
--- sem1Ball/sem1Ball/Assets/Scripts/Elevator.cs
+++ sem1Ball/sem1Ball/Assets/Scripts/Elevator.cs
@@ -4,41 +4,48 @@ using UnityEngine;
 public class Elevator : MonoBehaviour
 {
     AudioSource music;
-    public bool isUp;
+    public bool isUp = false;
+    public bool isOn = false;
+    public bool isEnd = false;
+    public float max = 1.5f;
+    public float min = 0f;
+
     private void Start()
     {
-        ElevatorMoveing(0.5f);
+        ElevatorMoveing(1f);
     }
+
     public void ElevatorMoveing(float speed)
     {
         StartCoroutine(ElevatorAnim(speed));
+    }
+    public void TurnOn()
+    {
+        isOn = true;
     }
 
     IEnumerator ElevatorAnim(float speed)
     {
         while (true)
         {
-            if (transform.position.y <= 0.96)
-            {
-                transform.position += new Vector3(0, speed * Time.deltaTime);
-                Debug.Log("Up");
-            }
-            if (transform.position.y >= 0.97)
-            {
-                transform.position -= new Vector3(0, speed * Time.deltaTime);
-                Debug.Log("Down");
-            }
-            //transform.position += new Vector3(0, speed * Time.deltaTime);
-            Debug.Log("End");
+            if (isUp && isOn) moveUp(speed);
+            else moveDown(speed);
             yield return null;
         }
-        yield return null;
-/*        while (!isUp)
-        {
-            transform.position -= new Vector3(0, speed * Time.deltaTime);
-            yield return null;
-        }
-        //yield return null;*/
-
     }
+    void moveUp(float speed)
+    {
+        if (transform.localPosition.y <= max)
+            transform.localPosition += new Vector3(0, speed * Time.deltaTime);
+        if (transform.localPosition.y >= max && isEnd)
+        {
+            FindObjectOfType<Restart>().Load();
+        }
+    }
+    void moveDown(float speed)
+    {
+        if (transform.localPosition.y >= min)
+            transform.localPosition -= new Vector3(0, speed * Time.deltaTime);
+    }
+
 }
